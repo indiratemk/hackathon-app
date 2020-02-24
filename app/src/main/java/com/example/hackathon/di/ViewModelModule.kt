b53bf98aec138bca9a075b26af8363cb7c53222f
Indira <temkesheva.i@chocolife.kz>
2020-02-24 20:38:00 +0600

    add sign in view model to module for injecting

--- app/src/main/java/com/example/hackathon/di/ViewModelModule.kt
+++ app/src/main/java/com/example/hackathon/di/ViewModelModule.kt
@@ -1,10 +1,12 @@
 package com.example.hackathon.di
 
 import com.example.hackathon.domain.AuthRepository
-import com.example.hackathon.presentation.SignUpViewModel
+import com.example.hackathon.presentation.sign_up.SignUpViewModel
+import com.example.hackathon.presentation.sign_in.SignInViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { SignUpViewModel(get() as AuthRepository) }
+    viewModel { SignInViewModel(get() as AuthRepository) }
 }
