49d2b531e760b51a9ffa9d482d4c3937275c0418
Indira <temkesheva.i@chocolife.kz>
2020-02-28 00:07:09 +0600

    add qr scanner screen

---
+++ app/src/main/java/com/example/hackathon/presentation/qr_scanner/QRScannerActivity.kt
@@ -0,0 +1,68 @@
+package com.example.hackathon.presentation.qr_scanner
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.PointF
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView
+import com.example.hackathon.R
+import com.example.hackathon.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_qr_scanner.*
+
+// TODO может сделать расширение для QRCodeReaderView чтобы не тащить весь класс
+class QRScannerActivity : BaseActivity(), QRCodeReaderView.OnQRCodeReadListener {
+
+    private var isFlashEnabled = false
+    private var isFrontCamera = false
+
+    override fun layoutId(): Int {
+        return R.layout.activity_qr_scanner
+    }
+
+    companion object {
+        fun startActivity(context: Context) {
+            val intent = Intent(context, QRScannerActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.DarkTheme)
+        super.onCreate(savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        qrScannerView.setOnQRCodeReadListener(this)
+        qrScannerView.setAutofocusInterval(1000L)
+
+        ivClose.setOnClickListener { finish() }
+        ivFlash.setOnClickListener {
+            isFlashEnabled = !isFlashEnabled
+            ivFlash.setImageResource(if (isFlashEnabled) R.drawable.ic_flash else R.drawable.ic_flash_off)
+            qrScannerView.setTorchEnabled(isFlashEnabled)
+        }
+        ivCameraRotation.setOnClickListener {
+            isFrontCamera = !isFrontCamera
+            qrScannerView.switchCameraFace()
+            ivFlash.visibility = if (isFrontCamera) View.GONE else View.VISIBLE
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        qrScannerView.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        qrScannerView.stopCamera()
+    }
+
+    override fun onQRCodeRead(text: String?, points: Array<out PointF>?) {
+        Log.d("taaaaag", "text: ${text}")
+//        finish()
+    }
+}
