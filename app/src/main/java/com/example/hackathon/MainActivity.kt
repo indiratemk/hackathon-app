56283c8785a0fc77d43d047fbd5bf95f7171a814
Indira <temkesheva.i@chocolife.kz>
2020-02-28 00:07:52 +0600

    add transition to qr scanner screen

--- app/src/main/java/com/example/hackathon/MainActivity.kt
+++ app/src/main/java/com/example/hackathon/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.hackathon
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.hackathon.presentation.qr_scanner.QRScannerActivity
 import com.example.hackathon.presentation.sign_in.SignInActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,5 +17,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnClick.setOnClickListener { SignInActivity.startActivity(this) }
+
+        btnQRScanner.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1011)
+            } else {
+                QRScannerActivity.startActivity(this)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1011) {
+            QRScannerActivity.startActivity(this)
+        }
     }
 }
