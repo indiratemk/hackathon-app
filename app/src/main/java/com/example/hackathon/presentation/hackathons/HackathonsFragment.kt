fff1ce0a211d6ebcc1bd19fa95cb56a592d3c527
Indira <temkesheva.i@chocolife.kz>
2020-03-11 22:52:51 +0600

    add hackathons retrieving

--- app/src/main/java/com/example/hackathon/presentation/hackathons/HackathonsFragment.kt
+++ app/src/main/java/com/example/hackathon/presentation/hackathons/HackathonsFragment.kt
@@ -1,11 +1,17 @@
 package com.example.hackathon.presentation.hackathons
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.example.hackathon.R
+import com.example.hackathon.util.state.State
+import com.example.hackathon.util.ui.UIUtil
+import kotlinx.android.synthetic.main.fragment_hackathons.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class HackathonsFragment : Fragment() {
 
@@ -18,11 +24,37 @@ class HackathonsFragment : Fragment() {
         }
     }
 
+    private val hackathonsViewModel: HackathonsViewModel by viewModel()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_hackathons, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeObservers()
+        btnHackathons.setOnClickListener {
+            hackathonsViewModel.getHackathons()
+        }
+    }
+
+    private fun subscribeObservers() {
+        hackathonsViewModel.hackathons.observe(viewLifecycleOwner, Observer { dataState ->
+            when (dataState) {
+                is State.Success -> {
+                    Log.d("taaaaag", "hackathons: ${dataState.data?.results}")
+                }
+                is State.NetworkError -> {
+                    UIUtil.showErrorMessage(requireActivity(), dataState.message)
+                }
+                is State.BackendError -> {
+                    UIUtil.showErrorMessage(requireActivity(), dataState.message)
+                }
+            }
+        })
     }
 }
